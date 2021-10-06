13ff20b02b1a58f9d494c4e84eb2381ee3a90a89
unknown <dlrmsgnl620@naver.com>
2021-10-06 23:25:40 +0900

    Gugudan_ex

---
+++ src/Gugudan_ex2.java
@@ -0,0 +1,16 @@
+public class Gugudan_ex2 {
+	public static void main(String[] args) {
+		//6단 구현
+//		int i = 1;
+//		while(i < 10) {
+//			i = i+1;
+//			System.out.println(6*i);
+		
+		//7단 구현
+		for(int i = 1; i<10; i++) {
+			System.out.println(7*i);
+		}
+	}
+}
+
+		
