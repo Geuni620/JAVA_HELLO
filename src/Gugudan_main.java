64bad9ac333ac167a9423cb9f388587d882e2b85
unknown <dlrmsgnl620@naver.com>
2021-10-08 21:31:49 +0900

    Last subject

---
+++ src/Gugudan_main.java
@@ -0,0 +1,8 @@
+public class Gugudan_main {
+	public static void main(String[] args) {
+		for(int i= 2; i < 10; i++) {
+			int[] result = Gugudan_ex3.calculate(i);
+			Gugudan_ex3.print(result);
+		}
+	}
+}
