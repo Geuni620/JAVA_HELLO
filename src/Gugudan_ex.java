dbd1ac2aa9d10eefcb1c016598495741062cbe07
unknown <dlrmsgnl620@naver.com>
2021-10-11 18:37:17 +0900

    method_ex

--- src/Gugudan_ex.java
+++ src/Gugudan_ex.java
@@ -1,13 +1,20 @@
 public class Gugudan_ex {
-	public static void main(String[] args) {
+	public static int[] calculate(int times) {
 		int[] result = new int[9];
-	for(int j = 0; j < result.length; j++) {
-		for(int i=0; i < result.length; i++) {
-			result[i] = (j+1) * (i+1);
+		for(int i = 0; i < result.length; i++) {
+			result[i] = times * (i+1);
 		}
-		for(int i=0; i < result.length; i++) {
+		return result;
+	}
+	public static void print(int[] result) {
+		for(int i = 0; i < result.length; i++) {
 			System.out.println(result[i]);
-			}
+		}
+	}
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) {
+			int[] result = calculate(i);
+			print(result);	
 		}
 	}
 }
