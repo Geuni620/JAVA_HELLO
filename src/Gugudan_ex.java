7d7280a21a594d2e5f0af0b854140b4bb1b593e4
unknown <dlrmsgnl620@naver.com>
2021-10-11 17:35:54 +0900

    duplication for

--- src/Gugudan_ex.java
+++ src/Gugudan_ex.java
@@ -1,21 +1,13 @@
-import java.util.Scanner;
 public class Gugudan_ex {
 	public static void main(String[] args) {
-		System.out.println("사용자가 출력할 값은 ? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println("사용자가 선택한 값은 ? : " + number);
-		
-		if ( number < 2 ) {
-			System.out.println("잘못 입력하셨습니다");
+		int[] result = new int[9];
+	for(int j = 0; j < result.length; j++) {
+		for(int i=0; i < result.length; i++) {
+			result[i] = (j+1) * (i+1);
 		}
-		else if (number > 10) {
-			System.out.println("또 잘못 입력하셨어요?");
-		}
-		else { 
-			for(int i=1; i < 10; i++) {
-				System.out.println(number*i);
-			}			
+		for(int i=0; i < result.length; i++) {
+			System.out.println(result[i]);
+			}
 		}
 	}
 }
