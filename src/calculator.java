bdf10420b72522e4541987da7822da7374a71835
unknown <dlrmsgnl620@naver.com>
2021-10-16 21:23:15 +0900

    2개의 값을 입력 받아 사칙연산 계산

--- src/calculator.java
+++ src/calculator.java
@@ -1,9 +1,15 @@
-//계산기 구현 
+//계산기 구현
+import java.util.Scanner;
 public class calculator {
 	public static void main(String[] args) {
-		System.out.println("덧셈 : " + (3+4));
-		System.out.println("뺄셈 : " + (3-4));
-		System.out.println("곱셈 : " + (3*4));
-		System.out.println("나눗셈 : " + (3/4));
+		System.out.println("사용자가 입력한 값은  : ");
+		Scanner scanner = new Scanner(System.in);
+		int first = scanner.nextInt(); //첫 번째 값 저장 
+		
+		//TODO 두 번째 값을 입력 받아 저장
+		int second = scanner.nextInt(); //두 번째 값 저장
+		
+		//TODO 입력 받은 두 값에 대한 사칙연산을 계산해 출력
+		System.out.println(first + second);
 	}
 }
