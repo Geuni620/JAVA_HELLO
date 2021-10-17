3ab12c39a1b75629e5d8aae9622e1d0810e29bd2
unknown <dlrmsgnl620@naver.com>
2021-10-17 18:48:15 +0900

    세개의 수 사칙연산

--- src/calculator.java
+++ src/calculator.java
@@ -2,34 +2,36 @@
 import java.util.Scanner;
 public class calculator {
 	public static void main(String[] args) {
-		System.out.println("사용자가 입력한 값은  : ");
 		Scanner scanner = new Scanner(System.in);
-		int first = scanner.nextInt(); //첫 번째 값 저장 
+		System.out.println("첫 번째 입력 값 : ");
+		int first = scanner.nextInt();
 		
-		//사칙연산 기호 추가
+		System.out.println("사칙연산 기호 : ");
 		String symbol = scanner.next();
 		
-		//TODO 두 번째 값을 입력 받아 저장
-		int second = scanner.nextInt(); //두 번째 값 저장
+		System.out.println("두 번째 입력 값 : ");
+		int second = scanner.nextInt();
 		
-		//TODO 입력 받은 두 값에 대한 사칙연산을 계산해 출력
-		//System.out.println(first + second);
+		System.out.println("사칙연산 기호 : ");
+		symbol = scanner.next();
 		
+		System.out.println("세 번째 입력 값 : ");
+		int third = scanner.nextInt();
 		
-		if("+".equals(symbol)) {
-			System.out.println(first + second);
-		}
-		else if("-".equals(symbol)) {
-			System.out.println(first - second);
-		}
-		else if("*".equals(symbol)) {
-			System.out.println(first * second);
-		}
-		else if("/".equals(symbol)) {
-			System.out.println(first / second);
-		}
-		else {
-			System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다");	
-		}
+				if("+".equals(symbol)) {
+					System.out.println("덧셈" + (first + second + third));
+				}
+				else if ("-".equals(symbol)) {
+					System.out.println("뺄셈" + (first - second - third));
+				}
+				else if("*".equals(symbol)) {
+					System.out.println("곱셈" + (first * second * third));
+				}
+				else if("/".equals(symbol)) {
+					System.out.println("나눗셈" + (first / second / third));
+				}
+				else {
+					System.out.println("사용자가 잘못 입력하셨습니다.");
+				}
 	}
 }
