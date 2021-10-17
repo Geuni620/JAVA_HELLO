346e30dd758236446667f0db0f97ce8c7aadee13
unknown <dlrmsgnl620@naver.com>
2021-10-17 19:08:07 +0900

    계산기 기본 기능 구현 완료

--- src/calculator.java
+++ src/calculator.java
@@ -5,33 +5,39 @@ public class calculator {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("첫 번째 입력 값 : ");
 		int first = scanner.nextInt();
-		
+
+		int result = first;  //결과를 받을 변수를 하나 만듦
+		while(true) {
 		System.out.println("사칙연산 기호 : ");
 		String symbol = scanner.next();
+		
+		if("quit".equals(symbol)) {
+			System.out.println("최종 결과 값 : " + (result));
+			break;
+		}
 		
 		System.out.println("두 번째 입력 값 : ");
 		int second = scanner.nextInt();
 		
-		System.out.println("사칙연산 기호 : ");
-		symbol = scanner.next();
-		
-		System.out.println("세 번째 입력 값 : ");
-		int third = scanner.nextInt();
-		
-				if("+".equals(symbol)) {
-					System.out.println("덧셈" + (first + second + third));
-				}
-				else if ("-".equals(symbol)) {
-					System.out.println("뺄셈" + (first - second - third));
-				}
-				else if("*".equals(symbol)) {
-					System.out.println("곱셈" + (first * second * third));
-				}
-				else if("/".equals(symbol)) {
-					System.out.println("나눗셈" + (first / second / third));
-				}
-				else {
-					System.out.println("사용자가 잘못 입력하셨습니다.");
-				}
+		if("+".equals(symbol)) {
+			result = result + second;
+			System.out.println("덧셈" + (result));
+		}
+		else if ("-".equals(symbol)) {
+			result = result - second;
+			System.out.println("뺄셈" + (result));
+		}
+		else if("*".equals(symbol)) {
+			result = result * second;
+			System.out.println("곱셈" + (result));
+		}
+		else if("/".equals(symbol)) {
+			result = result / second;
+			System.out.println("나눗셈" + (result));
+		}
+		else {
+			System.out.println("사용자가 잘못 입력하셨습니다.");
+			}
+		}
 	}
 }
