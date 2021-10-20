60ef143d6576911e3d8c98d54e2819f1a224d07d
unknown <dlrmsgnl620@naver.com>
2021-10-20 22:28:28 +0900

    메소드를 분리해 계산기 기능 구현

--- src/calculator.java
+++ src/calculator.java
@@ -1,45 +1,65 @@
 import java.util.Scanner;
 public class calculator{
-	public static void main(String[] args) {
-		System.out.println("첫 번째 입력할 숫자는 ? :");
-		Scanner scanner = new Scanner(System.in);
+	static int getFirstValue(Scanner scanner) {
+		System.out.println("첫 번째 입력 값 : ");
 		int first = scanner.nextInt();
-		System.out.println("첫 번째 입력한 값은 : " + first);
-		
+		System.out.println(first);
+		return first;
+	}
+	
+	static String getSymbol(Scanner scanner) {
+		System.out.println("기호 : ");
+		String symbol = scanner.next();
+		System.out.println(symbol);
+		return symbol;
+	}
+	
+	static int getSecondValue(Scanner scanner) {
+		System.out.println("두 번째 입력 값 : ");
+		int second = scanner.nextInt();
+		System.out.println(second);
+		return second;
+	}
+	
+	static int calculator(int first, String symbol, int second) {
+		int result = 0;
+		if(symbol.equals("+")) {
+			result = first + second;
+			System.out.println("덧셈 : " + (result));
+		} else if (symbol.equals("-")) {
+			result = first - second;
+			System.out.println("뺄셈 : " + (result));
+		} else if (symbol.equals("*")) {
+			result = first * second;
+			System.out.println("곱셈 : " + (result));
+		} else if (symbol.equals("/")) {
+			result = first / second;
+			System.out.println("나눗셈 : " + (result));
+		} else {
+			System.out.println("사용자가 잘못 입력하셨습니다." );
+		}
+		return result;
+	}
+	static void print(int result) {
+		System.out.println("최종 결과 값 : "+ result);
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+	
+		int first = getFirstValue(scanner);
+	
 		int result = first;
 		while(true) {
-		System.out.println("입력할 사칙연산 기호는 ? :");
-		String symbol= scanner.next();
-		System.out.println("사용자가 입력한 사칙연산 기호 :" + symbol);
-		
-		if("quit".equals(symbol)) {
-			System.out.println("최종 결과 값은 : " + result);
-			break;
+			String symbol = getSymbol(scanner);
+				
+			if("quit".equals(symbol)) {
+				System.out.println(result);
+				break;
 			}
-		
-		System.out.println("두 번째 입력할 숫자는 ? :");
-		int second = scanner.nextInt();
-		System.out.println("두 번째 입력한 값은 : " + second);
-		
-		if("+".equals(symbol)) {
-			result = first + second;
-			System.out.println(result);
-		}
-		else if("-".equals(symbol)) {
-			result = result - second;
-			System.out.println(result);
-		}
-		else if("*".equals(symbol)) {
-			result = result * second;
-			System.out.println(result);
-		}
-		else if("/".equals(symbol)) {
-			result = result / second;
-			System.out.println(result);
-		}
-		else {
-			System.out.println("사용자가 잘못 입력하셨습니다");
-			}
+			int second = getSecondValue(scanner);
+			
+			result = calculator(result, symbol, second);
 		}
 	}
 }
