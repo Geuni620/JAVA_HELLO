463d39ee94066254fbde06ba401244845ff9cd87
unknown <dlrmsgnl620@naver.com>
2021-10-25 15:44:06 +0900

    import 수정 + type 설정

--- src/Lottery.java
+++ src/Lottery.java
@@ -1,9 +1,13 @@
+import java.util.Random;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class Lottery{
 	private static final int NUMBER_OF_RANDOM_NUMBERS = 6;
 	private static final int MAX_RANDOM_NUMBER = 49;
 	
 	public static void main(String[] args) {
-		SortedSet randomNumbers = new TreeSet();
+		SortedSet<Integer> randomNumbers = new TreeSet<Integer>();
 		Random random = new Random();
 		while (randomNumbers.size() < NUMBER_OF_RANDOM_NUMBERS) {
 			Integer randomNumber = new Integer(random.nextInt(MAX_RANDOM_NUMBER) + 1);
